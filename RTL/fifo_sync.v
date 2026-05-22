9b5a9fa366d199aa858d6810b64012f212bf4504
Bhavyas15 <139043257+Bhavyas15@users.noreply.github.com>
2026-05-22 20:00:14 +0100

    Update fifo_sync.v

--- RTL/fifo_sync.v
+++ RTL/fifo_sync.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 22.05.2026 15:35:32
-// Design Name: 
-// Module Name: fifo_sync
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module fifo_sync#(
   parameter depth=8,
