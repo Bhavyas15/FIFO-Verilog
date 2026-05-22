d9081a3a7a7421f4a8f30ef1d3ef27ac3fd0de0d
Bhavyas15 <139043257+Bhavyas15@users.noreply.github.com>
2026-05-22 20:00:51 +0100

    Update tb_fifo_async.v

--- Testbench/tb_fifo_async.v
+++ Testbench/tb_fifo_async.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 22.05.2026 17:51:09
-// Design Name: 
-// Module Name: tb_async
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module tb_fifo_async(
     );
