f784eeeddebdc445a93c4e9420add6902a019ef0
Bhavyas15 <139043257+Bhavyas15@users.noreply.github.com>
2026-05-22 20:01:10 +0100

    Update tb_fifo_sync.v

--- Testbench/tb_fifo_sync.v
+++ Testbench/tb_fifo_sync.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 22.05.2026 15:37:14
-// Design Name: 
-// Module Name: tb_fifo_sync
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module tb_fifo_sync(
     );
